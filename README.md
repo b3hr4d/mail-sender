54e4279c4a671a3fe51af70859f00b4ae2cddfa9
b3hr4d <behradmusiclove@yahoo.com>
2022-09-11 00:04:05 +0300

    simple mail sender

---
+++ README.md
@@ -0,0 +1,53 @@
+## Check Balance and send email
+
+check address balance every 10 seconds and if its larger than 1 ETH send email to you
+
+### clone this repo
+
+```
+git clone git@github.com:b3hr4d/mail-sender.git
+```
+
+### copy .env.example to .env and fill it
+
+```
+cp .env.example .env
+```
+
+### install dependencies
+
+```
+npm install
+```
+
+or
+
+```
+yarn
+```
+
+### run
+
+```
+node index.js
+```
+
+### run in background
+
+```
+nohup node index.js &
+```
+
+### stop
+
+```
+ps -ef | grep node
+
+kill -9 PID
+```
+
+### check log
+
+```
+tail -f nohup.out
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+const Web3 = require("web3")
+const nodemailer = require("nodemailer")
+require("dotenv").config()
+
+const currentDateTime = new Date().toLocaleString()
+
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.PROVIDER))
+
+const address = process.env.ADDRESS
+
+const checkBalance = async () => {
+  const balance = await web3.eth.getBalance(address)
+  const balanceInEth = web3.utils.fromWei(balance, "ether")
+  console.log(currentDateTime, balanceInEth)
+  if (balanceInEth > 100) {
+    sendEmail()
+  }
+}
+
+const sendEmail = () => {
+  clearInterval(interval)
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  })
+
+  const mailOptions = {
+    from: process.env.MAIL_USERNAME,
+    to: process.env.MAIL_TO,
+    subject: "Address balance is larger than 100 ETH",
+    text: `On ${currentDateTime} Address balance is larger than 100 ETH`,
+  }
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log("Email sent: " + info.response)
+    }
+  })
+}
+
+const interval = setInterval(checkBalance, 10000)
